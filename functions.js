47dc51b7d43c7aeb813ddf1536a6d01d28dbfa39
Johnathan Ackerman <johnny.ackerman@bowtaps.com>
2016-03-23 00:44:11 -0600

    added navigation bar
    -tried to do a bunch of other things, all of which failed like miserably

---
+++ functions.js
@@ -0,0 +1,18 @@
+function kiwiHeader() {
+   use document.write("<b> 
+        <!-- set favicon -->
+        <link rel=/"shortcut icon/" type=/"image/ico/" href=/"images/favicon.ico/" />
+
+    <html>
+        <head>
+            <link rel=/"stylesheet/" type=/"text/css/" href=/"mainStyle.css/">
+            <title>Johnny's Website Connection</title>
+        </head>
+            <header><h1>
+                <img src=/"images/Kiwi.png/" alt=/"Kiwi/" align=/"middle/" height=/"100/">
+                <br/> Johnny's Website Connection
+            </h1></header>
+            <hr />
+        
+    </b>");
+}
