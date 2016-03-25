5f1e0773e420a4cef6beeba5bc35982603a013b9
Johnathan Ackerman <johnny.ackerman@bowtaps.com>
2016-03-24 22:30:14 -0600

    finished website

--- functions.js
+++ functions.js
@@ -1,18 +1,13 @@
-function kiwiHeader() {
-   use document.write("<b> 
-        <!-- set favicon -->
-        <link rel=/"shortcut icon/" type=/"image/ico/" href=/"images/favicon.ico/" />
-
-    <html>
-        <head>
-            <link rel=/"stylesheet/" type=/"text/css/" href=/"mainStyle.css/">
-            <title>Johnny's Website Connection</title>
-        </head>
-            <header><h1>
-                <img src=/"images/Kiwi.png/" alt=/"Kiwi/" align=/"middle/" height=/"100/">
-                <br/> Johnny's Website Connection
-            </h1></header>
-            <hr />
-        
-    </b>");
+function navBar() {
+    document.write(
+       '<!-- Navigation Bar -->\
+        <div class="Menu">\
+            <ul class="menuBar">\
+                <li><a href="index.html"><img src="images/home.png" width=100% alt="home"></a></li>\
+                <br/>\
+                <li><a href="sandbox.html"><img src="images/sandBox.png" width=100% alt="sandbox"></a></li>\
+                <br/>\
+                <li><a href="crowdControl.html"><img src="images/crowd_control.png" width=100% alt="Crowd Control"></a></li>\
+            </ul>\
+        </div>');
 }
